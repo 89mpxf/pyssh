77fb5ec3da3eaae90fe8083c0e73251f295e6930
89mpxf <diamond061@outlook.com>
2022-12-09 10:39:18 -0500

    Finished protocol identity exchange portion of KEX

--- src/handshake/start.py
+++ src/handshake/start.py
@@ -6,3 +6,6 @@ from .version import server_version
 
 def handshake(sock: socket):
     sock.send(server_version())
+    client_version = sock.recv(2048).decode().strip()
+    print("Client Version: " + client_version)
+    
--- src/handshake/version.py
+++ src/handshake/version.py
@@ -2,4 +2,4 @@
 from platform import python_version_tuple
 
 def server_version() -> bytes:
-    return "SSH-2.0-Python_{}.{}.{}".format(*python_version_tuple()).encode()
+    return "SSH-2.0-Python_{}.{}.{}\r\n".format(*python_version_tuple()).encode()
